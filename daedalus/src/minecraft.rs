a0ff9275cbbfaf3788eb5e3bf3293211559b44e4
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-06-07 16:29:49 -0700

    move gradle specifier to lib.rs
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- daedalus/src/minecraft.rs
+++ daedalus/src/minecraft.rs
@@ -1,11 +1,9 @@
 use crate::modded::{Processor, SidedDataEntry};
-use crate::{download_file, Error};
+use crate::{download_file, Error, GradleSpecifier};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::fmt::Display;
-use std::str::FromStr;
 
 #[cfg(feature = "bincode")]
 use bincode::{Decode, Encode};
@@ -75,143 +73,6 @@ pub struct Version {
     pub java_profile: Option<MinecraftJavaProfile>,
 }
 
-#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
-#[derive(PartialEq, Eq, Debug, Clone, Default)]
-/// A specifier string for Gradle
-pub struct GradleSpecifier {
-    /// The groups of the artifact
-    pub group: String,
-    /// Artifact name
-    pub name: String,
-    /// Classifier of the artifact
-    pub classifier: Option<String>,
-    /// Version of the artifact
-    pub version: String,
-    /// File extension
-    pub extension: Option<String>,
-}
-
-impl GradleSpecifier {
-    /// Returns the filename of the artifact
-    pub fn filename(&self) -> String {
-        let ext = if let Some(ext) = &self.extension {
-            format!(".{}", ext)
-        } else {
-            "".to_string()
-        };
-        if let Some(classifier) = &self.classifier {
-            format!("{}-{}-{}{}", self.name, self.version, classifier, ext)
-        } else {
-            format!("{}-{}{}", self.name, self.version, ext)
-        }
-    }
-
-    /// returns the base path of the artifact
-    pub fn base(&self) -> String {
-        format!(
-            "{}/{}/{}",
-            self.group.replace(".", "/"),
-            self.name,
-            self.version
-        )
-    }
-
-    /// REturns the full path of the artifact
-    pub fn path(&self) -> String {
-        format!("{}/{}", self.base(), self.filename())
-    }
-}
-
-impl FromStr for GradleSpecifier {
-    type Err = Error;
-
-    fn from_str(specifier: &str) -> Result<Self, Self::Err> {
-        let at_parts = specifier.split('@').collect::<Vec<&str>>();
-
-        let mut parts = at_parts
-            .first()
-            .ok_or(Error::InvalidGradleSpecifier(specifier.to_string()))?
-            .split(':')
-            .collect::<VecDeque<&str>>();
-        let group = parts
-            .pop_front()
-            .ok_or(Error::InvalidGradleSpecifier(specifier.to_string()))?
-            .to_string();
-        let name = parts
-            .pop_front()
-            .ok_or(Error::InvalidGradleSpecifier(specifier.to_string()))?
-            .to_string();
-        let version = parts
-            .pop_front()
-            .ok_or(Error::InvalidGradleSpecifier(specifier.to_string()))?
-            .to_string();
-        let extension = if at_parts.len() == 2 {
-            Some("jar".to_string())
-        } else {
-            Some(at_parts[1].to_string())
-        };
-        let classifier = if let Some(cls) = parts.pop_front() {
-            Some(cls.to_string())
-        } else {
-            None
-        };
-        Ok(GradleSpecifier {
-            group,
-            name,
-            classifier,
-            version,
-            extension,
-        })
-    }
-}
-
-impl Display for GradleSpecifier {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let extension = if let Some(ext) = &self.extension {
-            if ext != "jar" {
-                format!("@{}", ext)
-            } else {
-                String::new()
-            }
-        } else {
-            String::new()
-        };
-
-        if let Some(classifier) = self.classifier.as_ref() {
-            write!(
-                f,
-                "{}:{}:{}:{}{}",
-                self.group, self.name, self.version, classifier, extension
-            )
-        } else {
-            write!(
-                f,
-                "{}:{}:{}{}",
-                self.group, self.name, self.version, extension
-            )
-        }
-    }
-}
-
-impl Serialize for GradleSpecifier {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(&self.to_string())
-    }
-}
-
-impl<'de> Deserialize<'de> for GradleSpecifier {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        String::deserialize(deserializer)?
-            .parse()
-            .map_err(serde::de::Error::custom)
-    }
-}
 
 #[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 #[derive(Serialize, Deserialize, Debug, Clone)]
